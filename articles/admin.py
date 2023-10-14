766ab66c85cef03788439dd9f567cef30a6b68c0
Leonardo Wildt <leonardod.trimarchi@gmail.com>
2023-10-14 17:16:39 -0400

    comment view

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from .models import Article, Comment
 
-from .models import Article
+class CommentInline(admin.TabularInline):  # new
+    model = Comment
+    extra = 0
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "body")
+    inlines = [
+        CommentInline,
+    ]
+    list_display = [
+        "title",
+        "body",
+        "author",
+    ]
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
